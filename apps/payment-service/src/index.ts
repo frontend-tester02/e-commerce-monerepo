a5cc1eeb516bc01511da5677c75653e3985c7ca6
Shokhrukh <shokhrukhnuriddinov@gmail.com>
2026-03-26 02:27:33 +0900

    E-commerce Application Accepting Payments

--- apps/payment-service/src/index.ts
+++ apps/payment-service/src/index.ts
@@ -2,6 +2,7 @@ import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth'
 import { authMiddleware } from './middleware/auth-middleware.js'
+import stripe from './utils/stripe.js'
 
 const app = new Hono()
 
@@ -15,11 +16,25 @@ app.get('/', c => {
 	})
 })
 
-app.get('/test', authMiddleware, c => {
-	return c.json({
-		message: 'Payment service is auth',
-		userId: c.get('userId'),
+app.post('/create-stripe-product', async c => {
+	const res = await stripe.products.create({
+		id: '123',
+		name: 'Test Product',
+		default_price_data: {
+			currency: 'usd',
+			unit_amount: 10 * 100,
+		},
 	})
+
+	return c.json(res)
+})
+
+app.get('/stripe-product-price', async c => {
+	const res = await stripe.prices.list({
+		product: '123',
+	})
+
+	return c.json(res)
 })
 
 const start = async () => {
@@ -31,7 +46,7 @@ const start = async () => {
 			},
 			info => {
 				console.log(`Payment service is running on port 8002`)
-			}
+			},
 		)
 	} catch (error) {
 		console.log(error)
